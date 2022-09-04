e5526f720a294ef312839f75735608f636cf4a33
PanosKikas <kikaspanagiotis@gmail.com>
2022-09-04 19:26:17 +0100

    Camera follow and fix recenter issue

--- Roof Rails Clone/Assets/Scripts/Pipe.cs
+++ Roof Rails Clone/Assets/Scripts/Pipe.cs
@@ -47,6 +47,11 @@ public class Pipe : MonoBehaviour
         }
              
         float cutScaleY = dir.magnitude * 0.5f;
+        if (cutScaleY <= 0.09f)
+        {
+            return;
+        }
+
         GameObject cutInstance  = Instantiate(PipePrefab, spawnPosition, transform.rotation);
         cutInstance.transform.localScale = new Vector3(transform.localScale.x, cutScaleY, transform.localScale.z);
         cutInstance.GetComponent<Rigidbody>().AddForce(-1 * cutInstance.transform.forward * cutForce, ForceMode.Impulse);
@@ -85,12 +90,12 @@ public class Pipe : MonoBehaviour
             return;
         }
 
-        if (transform.position.x == 0f)
+        if (transform.localPosition.x == 0f)
         {
             needRecenter = false;
             return;
         }
 
-        transform.position = Vector3.Lerp(transform.position, new Vector3(0f, transform.position.y, transform.position.z), Time.deltaTime * recenterSpeed);
+        transform.localPosition = Vector3.Lerp(transform.localPosition, new Vector3(0f, transform.localPosition.y, transform.localPosition.z), Time.deltaTime * recenterSpeed);
     }
 }
